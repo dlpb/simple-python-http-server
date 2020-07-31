78b5a0d065304ff6319e126893d2c6ce4a75b074
Daniel <daniel.b0@ntlworld.com>
2020-07-31 10:27:49 +0100

    Fixing dodgy characters

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python2
-​
+
 import BaseHTTPServer, SimpleHTTPServer
 import SocketServer
 import logging
-​
+
 PORT = 8080
-​
+
 class GetHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
-​
+
     def do_GET(self):
         logging.error("===== Begin Request =====")
         logging.error(self.headers)
@@ -16,13 +16,13 @@ class GetHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
         self.send_header('Content-type', 'application/json')
         self.send_header('Cache-Control', 'no-cache,no-store')
         self.end_headers()
-​
+
         page ='''{"status":"ok"}'''
-​
+
         self.wfile.write(page)
-​
-​
+
+
 Handler = GetHandler
 httpd = SocketServer.TCPServer(("", PORT), Handler)
-​
+
 httpd.serve_forever()
